f6cc77dfb9c46068a6bbd38318a6e180cad98bdd
mark7dev <mark7dev@hotmail.com>
2019-06-02 19:59:46 -0500

    configuring redux

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import store from './store';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        
-      </div>
+      <Provider store={store}>
+      
+      </Provider>
     );
   }
 }
